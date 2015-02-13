8b7d461bed2fb7b52c9ddcc2c7083b39ee321c8e
Filip Dupanović <filip.dupanovic@gmail.com>
2015-02-13 12:26:41 +0100

    Add support for populating and parsing command-line options from the config

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
+// Require dependencies.
 var config = require('npm_lazy/config');
+var pkg = require('./package.json');
+var program = require('commander');
+var server = require('npm_lazy/server');
+var utils = require('./lib/utils');
 
-require('npm_lazy/server')(config);
+// Setup program.
+program
+  .version(pkg.version)
+  .usage('[options]')
+  .description('Starts a dynamically configured npm_lazy server instance');
+
+// Populate and parse options.
+utils.configureProgramOptions(config, program);
+program.parse(process.argv);
+
+// Update configuration.
+utils.updateConfiguration(config, program);
+
+// Start server.
+server(config);
